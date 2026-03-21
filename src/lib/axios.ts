239e417c42c285a08bbe53099af0361fb2a68e50
Maxim Konoschenko <maksim08k@gmail.com>
2026-03-21 14:51:02 +0300

    Now we have a domain!

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://spark-news.ru/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
